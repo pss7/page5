1d67a24279670cec241dff09d29907a37e319826
pss7 <124497122+pss7@users.noreply.github.com>
2023-06-26 12:12:57 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(function () {
+
+
+    /* section1 */
+    $('.tab_content').hide();
+    $('.tab_content').first().show();
+    $('#section1 .top .tab li').click(function () {
+        var Idx = $(this).index();
+        $('.tab_content').hide();
+        $('.tab_content').eq(Idx).show();
+    });
+
+    $('#section1 .slick').slick({
+        variablewidth: true,
+        autoplay: true,
+        arrows: true,
+        prevArrow: $('#section1 .control .prev'),
+        nextArrow: $('#section1 .control .next'),
+        dots: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        zIndex: 1000,
+        pauseOnHover: false,
+        autoplaySpeed: 2500,
+        speed: 1000,
+
+    });
+
+    $('#section4 .slick').slick({
+        autoplay: true,
+        arrows: false,
+        dots: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        zIndex: 1000,
+        pauseOnHover: false,
+        autoplaySpeed: 2500,
+        speed: 1000,
+        vertical: true,
+    });
+
+});
