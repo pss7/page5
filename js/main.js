218f4db3af675944b26ce70b7f99b97de1b7deaf
pss7 <124497122+pss7@users.noreply.github.com>
2023-06-27 23:33:12 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -12,23 +12,60 @@ $(function () {
         $('.tab_content').eq(Idx).show();
     });
 */
-$('.tab_content').hide();
-$('.tab_content').first().show();
-    $('#section1 .slick').slick({
-        variablewidth: true,
-        autoplay: true,
-        arrows: true,
-        prevArrow: $('#section1 .control .prev'),
-        nextArrow: $('#section1 .control .next'),
-        dots: false,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        zIndex: 1000,
-        pauseOnHover: false,
-        autoplaySpeed: 2500,
-        speed: 1000,
+    /*
+    $('.tab_content').hide();
+    $('.tab_content').first().show();
+        $('#section1 .slick').slick({
+            variablewidth: true,
+            autoplay: true,
+            arrows: true,
+            prevArrow: $('#section1 .control .prev'),
+            nextArrow: $('#section1 .control .next'),
+            dots: false,
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            zIndex: 1000,
+            pauseOnHover: false,
+            autoplaySpeed: 2500,
+            speed: 1000,
+        });
+    
+    */
+
+    var ww = $(window).width();
+    var mySwiper = undefined;
+
+    function initSwiper() {
+
+        if (ww < 1025 && mySwiper == undefined) {
+            mySwiper = new Swiper("#section1 .swiper-container", {
+                slidesPerView: 'auto',
+                simulateTouch: true,
+                loop: true,
+                speed:800,
+                autoplay: {
+                    delay: 1500,
+                    disableOnInteraction: false,
+                },
+            });
+        } else if (ww >= 1025 && mySwiper != undefined) {
+            mySwiper.destroy();
+            mySwiper = undefined;
+        }
+    }
+
+    initSwiper();
+
+    $(window).on('resize', function () {
+        ww = $(window).width();
+        initSwiper();
     });
+
+
+
+
+
 
     $('#section4 .slick').slick({
         autoplay: true,
@@ -43,5 +80,4 @@ $('.tab_content').first().show();
         speed: 1000,
         vertical: true,
     });
-
 });
