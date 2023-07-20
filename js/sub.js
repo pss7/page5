c60d839b81b49abdc0f799a6eade48a123922450
pss7 <124497122+pss7@users.noreply.github.com>
2023-07-20 09:58:28 +0900

    Add files via upload

--- js/sub.js
+++ js/sub.js
@@ -6,7 +6,7 @@ $(function () {
 
     });
 
-    /* tab */
+    /* tab
     $('.tab_content').hide();
     $('.tab_content').first().show();
 
@@ -19,19 +19,32 @@ $(function () {
     });
 
     $('.click1').click(function () {
-        window.scrollTo({left:0, top:6300});
+        window.scrollTo({ left: 0, top: 6300 });
     });
     $('.click2').click(function () {
-        window.scrollTo({left:0, top:7400});
+        window.scrollTo({ left: 0, top: 7400 });
     });
     $('.click3').click(function () {
-        window.scrollTo({left:0, top:8600});
+        window.scrollTo({ left: 0, top: 8600 });
     });
     $('.click4').click(function () {
-        window.scrollTo({left:0, top:10800});
+        window.scrollTo({ left: 0, top: 10800 });
     });
     $('.click5').click(function () {
-        window.scrollTo({left:0, top:16500});
+        window.scrollTo({ left: 0, top: 16500 });
+    });
+ */
+
+    /* tab2 */
+    $('.tab_content2').hide();
+    $('.tab_content2').first().show();
+    $('.lesson_wrap .index_wrap .tab_list li').click(function () {
+
+        var Idx = $(this).index();
+
+        $('.tab_content2').hide();
+        $('.tab_content2').eq(Idx).show();
+        return false;
     });
 
 });
